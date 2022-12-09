a0db0c875d1ff3b4377d5cd23b7002ed4d178381
Adwin Mosses <100942168+Private-botz@users.noreply.github.com>
2022-12-09 22:45:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/Private-Botz/Elsa-Bot.git /Elsa-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /Elsa-Bot
 fi
 cd /LazyPrincess
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Elsa Bot...."
 python3 bot.py
