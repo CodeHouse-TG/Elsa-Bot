f36fd2d685b0057857881f06b67a3b281c90a138
Annuay Raj-k <100431877+anthappanorg@users.noreply.github.com>
2022-12-18 09:50:37 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('YO !')
-})
-app.listen(process.env.PORT || 3000)
