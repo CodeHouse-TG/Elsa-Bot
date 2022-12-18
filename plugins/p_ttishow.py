c9ac729ceabf3936bc2c052f931b6ab9af9da113
Annuay Raj-k <100431877+anthappanorg@users.noreply.github.com>
2022-12-18 10:54:03 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('🤥 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('🔔 Updates', url='https://t.me/Private_Botz')
+            InlineKeyboardButton('🔔 Updates', url='https://t.me/COdeHouse_TG')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
